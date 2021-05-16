af1227f77fb00667ab8a74989ccd167ac2a6b872
Camille Supery <csupery@gmail.com>
2021-05-16 20:08:19 +0200

    Failing with cocoon gem

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
 gem 'devise'
 gem 'pg'
+gem "cocoon"
 
 group :development, :test do
   gem 'pry-byebug'
--- app/models/winery.rb
+++ app/models/winery.rb
@@ -1,6 +1,8 @@
 class Winery < ApplicationRecord
-  has_many :appellation_wineries, dependent: :destroy
+  has_many :appellation_wineries, dependent: :destroy, inverse_of: :winery
   has_many :appellations, through: :appellation_wineries
+
+  accepts_nested_attributes_for :appellations, :allow_destroy => true
 
   validates :name, presence: true
 end
